1084638834741357ca2c644a0255c1cb49517a1b
Mohamed Morsi <777medooo@gmail.com>
2025-10-20 16:27:42 +0300

    feat: add email body builder

---
+++ SurveyBasket/Helpers/EmailBodyBuilder.cs
@@ -0,0 +1,26 @@
+﻿namespace SurveyBasket.Helpers;
+
+public static class EmailBodyBuilder
+{
+    /// TODO: Create The function that generate the email body
+    /// and take two parameters, one for template name and one 
+    /// as dict for replacing the placeholder with the value.
+    
+    public static string GenerateEmailBody(string template, Dictionary<string, string> templateModel)
+    {
+        // TODO: init var for the path of html template
+        var templatePath = $"{Directory.GetCurrentDirectory()}/Template/{template}.html";
+
+        // TODO: read the template using stream reader and close stream
+        var stream = new StreamReader(templatePath);
+        var body = stream.ReadToEnd();
+        stream.Close();
+
+        // TODO: replace each placeholder in the template with the corresponding value from the templateModel
+        foreach (var item in templateModel)
+            body.Replace(item.Key, item.Value);
+
+        return body;
+    }
+
+}
